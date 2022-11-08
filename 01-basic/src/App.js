e9c338763f78a7b355d28f75b4f0b5f37ce166af
unknown <sudinosg@protonmail.com>
2022-11-08 20:31:22 +0800

    create welcome

---
+++ 01-basic/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+      <React.Fragment>
+          <h1>Hello World</h1>
+          <p>Welcome to our first React App</p>
+      </React.Fragment>
+  );
+}
+
+export default App;
